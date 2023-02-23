66116e9b6e93e354be69d9c7ceb687458fe2dd80
Eelaaf Zahid <eelaaf.zahid@leidos.com>
2023-02-23 15:59:56 -0500

    made two pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,43 @@
-<script>
-  import Todos from "./components/Todos.svelte";
+<!-- Code referenced from https://svelte.dev/repl/8eb738732cf74edd86f680c53e6ba253?version=3.44.2 -->
 
-  let todos = [
-    { id: 1, name: "Create a Svelte starter app", completed: true },
-    { id: 2, name: "Create your first component", completed: true },
-    { id: 3, name: "Complete the rest of the tutorial", completed: false }
-  ];
+<script>
+  import startPage from "./components/startPage.svelte";
+  import collectionPage from "./components/collectionPage.svelte";
+
+
+  const pages = [startPage, collectionPage];
+
+  // The current page of our form.
+  let page = 0;
+
+  // The state of all of our pages
+  let pagesState = [];
+
+  // Our handlers
+  function onSubmit(values) {
+    if (page === pages.length - 1) {
+      // On our final page with POST our data somewhere
+      console.log('Submitted data: ', pagesState)
+    } else {
+      // If we're not on the last page, store our data and increase a step
+      pagesState[page] = values;
+      pagesState = pagesState; // Triggering update
+      page +=1;
+    }
+  }
+
+  function onBack(values) {
+    if (page === 0) return;
+		pagesState[page] = values;
+    pagesState = pagesState; // Triggering update
+    page -= 1;
+  }
 </script>
 
-<!-- In the script above, we pass our todos as #Properties to our #Component Todos.svelte-->
-
-
-<!-- Below, we render our todos #Properties, which are passed in from the App.svelte #Component -->
-<Todos {todos} />
+<!-- We display the current step here -->
+<svelte:component
+  this={pages[page]}
+  {onSubmit}
+  {onBack}
+  initialValues={pagesState[page]}
+  />
---
+++ src/components/startPage.svelte
@@ -0,0 +1,18 @@
+<!-- Code referenced from https://svelte.dev/repl/8eb738732cf74edd86f680c53e6ba253?version=3.44.2 -->
+
+<script>
+	import { createForm } from 'felte'
+	
+	export let initialValues;
+	export let onSubmit;
+	
+	const { form } = createForm({ onSubmit, initialValues })
+</script>
+
+<form use:form>
+	<label for=firstName>First name</label>
+	<input id=firstName name=firstName>
+	<label for=lastName>Last name</label>
+	<input id=lastName name=lastName><br>
+	<button type=submit>Next page</button>
+</form>
